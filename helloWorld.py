df17a3568da62ca6ea9d720fd6d0fa5fa9ef748e
xjc-xx <134748594@qq.com>
2021-01-05 18:44:24 +0800

    first commit

---
+++ helloWorld.py
@@ -0,0 +1,17 @@
+'''
+Author: CC-TSR
+Date: 2021-01-05 12:38:16
+LastEditTime: 2021-01-05 12:46:42
+LastEditors: xiejiancheng1999@qq.com
+Description: 
+FilePath: \teach_for_noobXi\helloWorld.py
+可以输入预定的版权声明、个性签名、空行等
+'''
+
+def sayHello():
+    print("Hello, World")
+
+if __name__ == '__main__':
+    sayHello()
+    
+
